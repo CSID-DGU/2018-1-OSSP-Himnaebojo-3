d803c850e91704c40e1dfa7dbea37764ba8756b1
Unknown <ljs921026@gmail.com>
2018-05-24 10:42:22 +0900

    디버깅 완료
    팩맨이 DeathState나 게임이 LevelBeginState로 넘어갈 시 프로그램이 죽는 버그 디버깅 완료.

--- pacman_2018/src/renderer.c
+++ pacman_2018/src/renderer.c
@@ -213,7 +213,7 @@ void draw_game_gameover(void)
 //
 //
 
-void draw_fruit_indicators(int currentLevel)//currentLevel이 0으로 입력되는 버그
+void draw_fruit_indicators(int currentLevel)
 {
 	if (currentLevel < 1)
 	{
