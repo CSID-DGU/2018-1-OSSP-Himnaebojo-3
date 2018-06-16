7993959a571aa9b2938957d973696747fc490c9f
오택완 <otw7777@naver.com>
2018-06-16 10:56:04 +0900

    Install SDL Library to execute game

---
+++ pacman_2018/InstallLib.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo apt install libsdl1.2debian libsdl-gfx1.2-5 libsdl-gfx1.2-dev libsdl-gfx1.2-doc libsdl-image1.2 libsdl-image1.2-dbg libsdl-image1.2-dev libsdl-mixer1.2 libsdl-mixer1.2-dbg libsdl-mixer1.2-dev libsdl-net1.2 libsdl-net1.2-dbg libsdl-net1.2-dev libsdl-sound1.2 libsdl-sound1.2-dev libsdl-ttf2.0-0 libsdl-ttf2.0-dev
